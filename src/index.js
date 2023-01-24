927892d45e76bca095aedc8090b188e68cd3d20c
Thomas <thomasmckenna12gmail.com>
2023-01-23 19:01:30 -0600

    restarted todo project

--- src/index.js
+++ src/index.js
@@ -7,15 +7,10 @@ const listItemsDisplay = document.querySelector(".list-items-display")
 const addBttn = document.querySelector(".add-bttn")
 
 
+
 addBttn.addEventListener("click", addToDo)
 
-
 function addToDo(e){
-    e.preventDefault()
-    const newToDoItem = document.createElement("li")
-    newToDoItem.id = "toDoItem"
-    newToDoItem.innerText = newToDo.value
-    listItemsDisplay.appendChild(newToDoItem)
 
 
 }
