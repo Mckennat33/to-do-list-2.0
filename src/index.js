d4d6d9eced855427c174966ba0300b763f23859b
Thomas <thomasmckenna12gmail.com>
2023-01-23 21:33:12 -0600

    added array push list item

--- src/index.js
+++ src/index.js
@@ -6,12 +6,31 @@ const toDoForm = document.querySelector(".list-items-div")
 const listItemsDisplay = document.querySelector(".list-items-display")
 const addBttn = document.querySelector(".add-bttn")
 
-
-
+const toDoItemsArray = [
+    {
+        id: new Date().getTime(),
+        text: "",
+        completed: false
+    }
+]
 addBttn.addEventListener("click", addToDo)
 
 function addToDo(e){
+    e.preventDefault()
+    const newToDoValue = newToDo.value
+    const toDoItemEntered = {
+        id: new Date().getTime(),
+        text: newToDoValue, 
+        completed: false
+    }
+    toDoItemsArray.push(toDoItemEntered)
 
+    const addedToDoItem = document.createElement("li")
+    addedToDoItem.classList.add("list-item")
+    addedToDoItem.innerHTML = newToDoValue
+
+    listItemsDisplay.appendChild(addedToDoItem)
+    console.log(toDoItemEntered)
 
 }
 
