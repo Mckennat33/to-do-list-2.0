e79eccb7018eb0260f4edccfd4c138b006020675
Thomas <thomasmckenna12gmail.com>
2023-01-25 19:44:16 -0600

    finished checkbox

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-
+// const { Checkbox } = require("semantic-ui-react")
 
 const newToDo = document.querySelector("input")
 
@@ -18,33 +18,39 @@ addBttn.addEventListener("click", addToDo)
 function addToDo(e){
     e.preventDefault()
     const newToDoValue = newToDo.value
+
+   // make a checkbox
+    const checkBox = document.createElement("input")
+    checkBox.type = "checkbox"
+    checkBox.id = "checkbox"
+    checkBox.classList.add("checkBox")
+    checkBox.addEventListener("click", function() {
+        // add line through text if checkbox is not checked
+        if (checkBox.checked == true) {
+            addedToDoItem.style.textDecoration = "line-through"
+        } else {
+            addedToDoItem.style.textDecoration = "none"
+        }
+    })
+
     const toDoItemEntered = {
         id: new Date().getTime(),
         text: newToDoValue, 
-        completed: false
+        checkBox: checkBox
     }
+    
     toDoItemsArray.push(toDoItemEntered)
 
     const addedToDoItem = document.createElement("li")
     addedToDoItem.classList.add("list-item")
     addedToDoItem.innerHTML = newToDoValue
 
-    // make a checkbox
-    const checkBox = document.createElement("input")
-    checkBox.type = "checkbox"
-    checkBox.id = "checkbox"
-    checkBox.classList.add("checkBox")
 
-    
+
 
 
     listItemsDisplay.appendChild(addedToDoItem)
     addedToDoItem.appendChild(checkBox)
-    
-
-
-
-
 }
 
 
